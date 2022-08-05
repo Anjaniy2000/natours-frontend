26aac5598e9d4763a67a0eb1cdaac544c537bdb4
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-08-05 23:09:36 +0530

    added tours component.
    added tours component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ToursComponent } from './tours/tours.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'tours', component: ToursComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
