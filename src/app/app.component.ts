c638089f4191992f74099d9be92d3bfd0e9cbf47
Sakshi Agrawal <shakshimnnit@gmail.com>
2022-08-05 23:49:40 +0530

    tours component opened
    tours component opened

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'natours-frontend';
+  opentours:boolean=false;
+  toursInfo(event:any){
+    if(event){
+this.opentours=true;
+    }
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  @Output() toursData:EventEmitter<any>= new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  goToTours(){
+ this.toursData.emit(true);
+  }
 }
