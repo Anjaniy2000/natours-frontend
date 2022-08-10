0569b75b057f1020e0a4ac6140cd35eede609ac7
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-08-10 23:31:58 +0530

    API call in tours component
    API call in tours component.

--- src/app/tours/tours.component.ts
+++ src/app/tours/tours.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ToursService } from '../services/tours.service';
 
 @Component({
   selector: 'app-tours',
   templateUrl: './tours.component.html',
-  styleUrls: ['./tours.component.css']
+  styleUrls: ['./tours.component.css'],
 })
 export class ToursComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private tours: ToursService) {
+    this.getAllTours();
   }
 
+  ngOnInit(): void {}
+
+  getAllTours() {
+    this.tours.getAllTours().subscribe((data: any) => {
+      console.warn(data);
+    });
+  }
 }
