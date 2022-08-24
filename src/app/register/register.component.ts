74c00f35c308ddad2d868dde763029db9ab123c6
Sakshi Agrawal <shakshimnnit@gmail.com>
2022-08-24 23:23:34 +0530

    registeration done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Register } from '../models/register.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+ regData:Register=new  Register();
+  constructor(private reg:AuthService) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  registerCustomer(){
+    this.reg.register(this.regData);
+}
+
+saveName(data:any){
+  this.regData.name=data;
+}
+saveEmail(data:any){
+  this.regData.email=data;
+}
+savePassword(data:any){
+  this.regData.password=data;
+}
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Register } from '../models/register.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+  register(data:Register){
+    return this.http.post('http://localhost:8083/api/auth/register',data)
+  }
+}
